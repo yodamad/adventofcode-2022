ee93eac7d2ac8bcf9a42170e5be64e9038d0bbb1
yodamad <mvincent.it@gmail.com>
2022-12-19 17:26:43 +0100

    🏖  Day 14 (part 1)

---
+++ lib/src/main/kotlin/adventofcode/day14/Day14.kt
@@ -0,0 +1,79 @@
+package adventofcode.day14
+
+import adventofcode.shared.getLines
+import java.util.stream.IntStream
+
+class Day14 {
+
+    val DAY = "day14"
+    var map = mutableListOf<Point>()
+    val startingPoint = Point(500, 0)
+
+    fun printMap(file: String) {
+        val lines = file.getLines(DAY)
+        lines.forEach { l ->
+            val points = l.split(" -> ")
+                .map { it.split(",") }
+                .map { Point(it[0].toInt(), it[1].toInt(), "#") }
+            println(points)
+            map.addAll(points)
+
+            points.forEachIndexed { index, point ->
+                if (index < points.size-1) drawLine(point, points[index+1])
+            }
+            map = map.sortedBy { it.y }.sortedBy { it.x }.distinct().toMutableList()
+        }
+        println(map)
+
+        var moreMove = true
+        var turn = 1
+        var currentPoint = startingPoint
+        var sandBlocks = 0
+        while(moreMove) {
+            println("Current point is $currentPoint")
+            if (currentPoint.y > map.maxOf { it.y }) {
+                // Finish
+                moreMove = false
+            }
+            if (map.contains(Point(currentPoint.x, currentPoint.y+1))) {
+                if (map.contains(Point(currentPoint.x-1, currentPoint.y+1))) {
+                    if (map.contains(Point(currentPoint.x+1, currentPoint.y+1))) {
+                        map.add(Point(currentPoint.x, currentPoint.y, "o"))
+                        currentPoint = startingPoint
+                        sandBlocks++
+                    } else {
+                        currentPoint = Point(currentPoint.x+1, currentPoint.y+1)
+                    }
+                } else {
+                    currentPoint = Point(currentPoint.x-1, currentPoint.y+1)
+                }
+            } else {
+                currentPoint = Point(currentPoint.x, currentPoint.y+1)
+            }
+        }
+
+        println("$sandBlocks sand blocks")
+    }
+
+    private fun drawLine(start: Point, end: Point) {
+        if (start.x == end.x) {
+            IntStream.range(minOf(start.y, end.y)+1, maxOf(start.y, end.y)).forEach {
+                map.add(Point(start.x, it, "#"))
+            }
+        } else {
+            IntStream.range(minOf(start.x, end.x)+1, maxOf(start.x, end.x)).forEach {
+                map.add(Point(it, start.y, "#"))
+            }
+        }
+    }
+}
+
+data class Point(val x: Int, val y: Int, val type: String = "") {
+    override fun toString(): String {
+        return "($x,$y)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is Point && other.x == x && other.y == y
+    }
+}
---
+++ lib/src/test/kotlin/adventofcode/day14/Day14Test.kt
@@ -0,0 +1,19 @@
+package adventofcode.day14
+
+class Day14Test {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            //Day14Test().sample()
+            Day14Test().map()
+        }
+    }
+
+    fun sample() {
+        Day14().printMap("sample")
+    }
+
+    fun map() {
+        Day14().printMap("map")
+    }
+}
